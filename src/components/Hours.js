32cff92a1fdf35ed84a54644f0fa1eea96a6e2cd
ingdanielbs <74162349+ingdanielbs@users.noreply.github.com>
2024-08-28 19:09:17 +0000

    chore: Update package name to "reto-codigofacilito" and add Hours component to index page

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,38 @@
 // Placeholder to host a component
+/*  El refugio abre de 10 a.m. a 4 p.m. de lunes a viernes, y de 9 a.m. a 8 p.m. los sábados y domingos. */
+/* Añadirás el código necesario para mostrar las horas del día actual de la semana. */
+import React from 'react';
+
+const Hours = () => {
+    const date = new Date();
+    const currentDay = date.getDay();
+    const currentHour = date.getHours();
+    let hours = 'Cerrado';
+    let openHour, closeHour;
+
+    if (currentDay >= 1 && currentDay <= 5) {
+        openHour = 10;
+        closeHour = 16;
+    } else {
+        openHour = 9;
+        closeHour = 20;
+    }
+
+    if (currentHour >= openHour && currentHour < closeHour) {
+        hours = 'Abierto';
+    }
+
+    const hoursStyle = {
+        color: hours === 'Abierto' ? 'green' : 'red'
+    };
+
+    return (
+        <div>
+            <div style={hoursStyle}>{hours}</div>
+            <div>Horas de apertura: {openHour} a.m. - {closeHour - 1} p.m.</div>
+        </div>
+    );
+}
+
+export default Hours;
+
